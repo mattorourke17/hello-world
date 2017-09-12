709172a75e7580efb99f4f0c20e6ad55d4b851da
Matt O'Rourke <mattorourke41@gmail.com>
2017-09-12 10:59:12 -0700

    First commit! basic helloworld file

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# a quick hello-world program to try to learn about git and github
+
+N = 6.0
+if N % 2.0 == 0.0:
+    print "Hello world! This number is even"
+
+if N % 2.0 != 0.0:
+    print "Hello world! this number is odd"
